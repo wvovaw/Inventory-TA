5cc6c6d6b471a71e8513663e91f7ffdc0e8a1387
wvovaw <vladimir5313375015@gmail.com>
2022-12-11 19:10:40 +0500

    mock layout with dark/light colorscheme toggle

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+const dark = ref<boolean>(false);
+const colorscheme = computed(() => (dark.value ? "dark" : "light"));
+</script>
+
+<template>
+  <div :class="{ dark }" class="">
+    <div
+      class="w-full h-screen flex flex-col gap-5 p-5 bg-slate-100 dark:bg-neutral-900 transition-colors duration-200"
+    >
+      <main class="h-[85%] flex flex-row gap-5">
+        <div
+          class="w-1/3 p-3 overflow-hidden rounded-lg bg-slate-300 dark:bg-neutral-800 transition-colors duration-200"
+        >
+          <div
+            class="w-full h-[50%] flex justify-center items-center overflow-hidden rounded-lg"
+          >
+            <img
+              src="https://picsum.photos/800/800/?grayscale=1"
+              class="shrink-0 min-w-full min-h-full"
+            />
+          </div>
+          <div class="px-5 mt-5 flex flex-col gap-2">
+            <div class="w-full h-8 rounded-lg bg-slate-400"></div>
+            <div class="w-16 h-3 rounded-lg bg-slate-400"></div>
+            <div class="w-32 h-3 rounded-lg bg-slate-400"></div>
+            <div class="w-24 h-3 rounded-lg bg-slate-400"></div>
+            <div class="w-16 h-3 rounded-lg bg-slate-400"></div>
+            <div class="w-32 h-3 rounded-lg bg-slate-400"></div>
+            <div class="w-52 h-3 rounded-lg bg-slate-400"></div>
+            <div class="w-52 h-3 rounded-lg bg-slate-400"></div>
+            <div
+              class="w-52 h-3 mt-6 self-center rounded-lg bg-slate-400"
+            ></div>
+          </div>
+        </div>
+        <div
+          class="w-full h-full rounded-lg bg-slate-300 dark:bg-neutral-800 transition-colors duration-200"
+        ></div>
+      </main>
+      <div
+        class="h-[15%] p-4 rounded-lg bg-slate-300 dark:bg-neutral-800 transition-colors duration-200"
+      >
+        <div
+          class="w-full h-full rounded-lg flex justify-center items-center text-2xl text-teal-900 dark:text-teal-200 font-semibold bg-slate-400 dark:bg-neutral-600 transition-colors duration-200"
+          @click="dark = !dark"
+        >
+          {{ colorscheme }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
